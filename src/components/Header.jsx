31c031891fc3fd62ae4e58c0d1f9cf17b533d6d5
yeon <ohy9205@naver.com>
2022-12-18 20:00:22 +0900

    Header 스타일 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import style from "./Header.module.css";
+import { BsFillSunFill } from "react-icons/bs";
 
 export default function Header() {
   return (
     <header>
-      <button>mode</button>
-      <nav>
-        <ul>
-          <li>All</li>
-          <li>Active</li>
-          <li>Completed</li>
-        </ul>
-      </nav>
+      <button className={style.mode}>
+        <BsFillSunFill />
+      </button>
+      <ul className={style.filter}>
+        <li className={style.on}>All</li>
+        <li>Active</li>
+        <li>Completed</li>
+      </ul>
     </header>
   );
 }

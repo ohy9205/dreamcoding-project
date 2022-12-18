4ad4be70e8ac1c539465db43ea5c7c6b76c700d4
yeon <ohy9205@naver.com>
2022-12-18 19:10:26 +0900

    ADD 마크업

---
+++ src/components/Add.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Add() {
+  return (
+    <footer>
+      <form>
+        <input type="text" placeholder="Add Todo" />
+        <button type="submit">Add</button>
+      </form>
+    </footer>
+  );
+}

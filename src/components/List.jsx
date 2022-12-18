550255ae1eaeec16da00a96887b065259f19d197
yeon <ohy9205@naver.com>
2022-12-18 20:21:57 +0900

    List 스타일 적용, 마크업 변경

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,21 +1,37 @@
 import React from "react";
+import style from "./List.module.css";
+import { MdDeleteForever } from "react-icons/md";
 
 export default function List() {
   return (
     <main>
       <ul>
         <li>
-          <input type="checkbox" value="test" />
-          test
-          <button>삭제</button>
+          <div>
+            <input type="checkbox" value="test" id="test" />
+            <label for="test">test</label>
+          </div>
+          <button className={style.remove}>
+            <MdDeleteForever />
+          </button>
         </li>
         <li>
-          <input type="checkbox" value="abc" />
-          abc <button>삭제</button>
+          <div>
+            <input type="checkbox" value="abc" id="abc" />
+            <label for="abc">abc</label>
+          </div>
+          <button className={style.remove}>
+            <MdDeleteForever />
+          </button>
         </li>
         <li>
-          <input type="checkbox" value="tdef" />
-          def <button>삭제</button>
+          <div>
+            <input type="checkbox" value="def" id="def" />
+            <label for="def">def</label>
+          </div>
+          <button className={style.remove}>
+            <MdDeleteForever />
+          </button>
         </li>
       </ul>
     </main>

42dfa34cfb78ea710fbbd84924664a4fd017af14
yeon <ohy9205@naver.com>
2022-12-18 19:05:46 +0900

    List 마크업

---
+++ src/components/List.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function List() {
+  return (
+    <main>
+      <ul>
+        <li>
+          test <button>완</button>
+        </li>
+        <li>
+          abc <button>완</button>
+        </li>
+        <li>
+          def <button>완</button>
+        </li>
+      </ul>
+    </main>
+  );
+}

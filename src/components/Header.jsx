7740d8d507a27e340a7376026cc8f2b88b1e931f
yeon <ohy9205@naver.com>
2022-12-18 19:02:06 +0900

    Header 마크업

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <button>mode</button>
+      <nav>
+        <ul>
+          <li>All</li>
+          <li>Active</li>
+          <li>Completed</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }

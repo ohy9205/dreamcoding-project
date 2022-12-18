032eaeec0e61e7375d15221b7194327e9e7a6969
yeon <ohy9205@naver.com>
2022-12-18 19:43:22 +0900

    App 스타일 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import Add from "./components/Add";
 import Header from "./components/Header";
+import List from "./components/List";
+import style from "./App.module.css";
+
+const dummy = [
+  {
+    id: 1,
+    todo: "밥먹기",
+    status: "todo",
+  },
+  {
+    id: 2,
+    todo: "밥먹기",
+    status: "todo",
+  },
+  {
+    id: 3,
+    todo: "밥먹기",
+    status: "active",
+  },
+  {
+    id: 4,
+    todo: "밥먹기",
+    status: "active",
+  },
+  {
+    id: 5,
+    todo: "밥먹기",
+    status: "done",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.App}>
       <Header />
+      <List />
+      <Add />
     </div>
   );
 }

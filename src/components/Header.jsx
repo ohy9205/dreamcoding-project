42f0e701422132114daf018779df7cbbe5e3d0a3
yeon <ohy9205@naver.com>
2022-12-22 13:09:15 +0900

    Header 스타일링 최종

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,20 +6,22 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="w-full p-4 border-b border-zinc-200 flex justify-between ">
-      <Link to="/" className="flex items-center">
-        <FiShoppingBag className="text-4xl text-sky-600" />
-        <h1 className="ml-2 text-4xl font-semibold text-sky-500">Shoppy</h1>
+      <Link to="/" className="flex items-center text-4xl  text-sky-500">
+        <FiShoppingBag />
+        <h1 className="ml-2 font-semibold">Shoppy</h1>
       </Link>
-      <nav className="w-1/4">
-        <ul className="flex items-center h-full justify-between text-xl font-semibold">
+      <nav>
+        <ul className="flex gap-4 items-center h-full justify-between text-xl font-semibold">
           <li>
-            <Link to="/products">Products</Link>
+            <Link k to="/products">
+              Products
+            </Link>{" "}
           </li>
           <li>
             <Link to="/carts">Carts</Link>
           </li>
           <li>
-            <Link to="/products/new">
+            <Link to="/products/new" className="text-2xl">
               <BsPencilFill />
             </Link>
           </li>
